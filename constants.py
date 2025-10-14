e6914af484293c853226617836119e9a3d351245
Per Kraulis <per.kraulis@gmail.com>
2025-10-14 11:19:48 +0200

    Added local tar dump script.

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ import string
 
 import babel.dates
 
-VERSION = (0, 11, 23)
+VERSION = (0, 11, 24)
 __version__ = ".".join([str(n) for n in VERSION])
 
 GITHUB_URL = "https://github.com/pekrau/chaos"
---
+++ cron/local_dump.py
@@ -0,0 +1,30 @@
+"chaos: Tar dump of local production directory."
+
+import datetime
+import os
+from pathlib import Path
+import sys
+import tarfile
+
+# Allow finding chaos modules.
+sys.path.insert(0, str(Path(sys.path[0]).parent))
+
+import constants
+
+
+def dump(source_dir, target_dir):
+    source_dir = Path(source_dir)
+    target_dir = Path(target_dir)
+    tarfilepath = target_dir / f"chaos_{datetime.date.today()}.tgz"
+
+    with tarfile.open(tarfilepath, mode="w:gz") as outfile:
+        for dirpath, dirnames, filenames in os.walk(source_dir):
+            abspath = Path(dirpath)
+            relpath = Path(dirpath).relative_to(source_dir)
+            for filename in filenames:
+                outfile.add(abspath.joinpath(filename),
+                            arcname=relpath.joinpath(filename))
+
+
+if __name__ == "__main__":
+    dump(os.environ["CHAOS_DIR"], "/home/pekrau/Dropbox/dumps/chaos")

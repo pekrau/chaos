0299b3b0e1eb1f17208a2d8622de3ac5c2da8b36
Per Kraulis <per.kraulis@gmail.com>
2026-07-22 17:41:40 +0200

    Forgot version update.

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ import re
 import string
 import zoneinfo
 
-VERSION = (1, 9, 1)
+VERSION = (1, 9, 2)
 __version__ = ".".join([str(n) for n in VERSION])
 
 GITHUB_URL = "https://github.com/pekrau/chaos"
